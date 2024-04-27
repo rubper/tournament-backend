8a32d4469f89eacb725322c5f93e822a4ee435e3
Ruben Perez <rub.per96@gmail.com>
2024-04-27 02:05:31 -0600

    fix: remove challonge module from stage module

--- src/modules/stage/stage.module.ts
+++ src/modules/stage/stage.module.ts
@@ -3,10 +3,9 @@ import { StageService } from './stage.service';
 import { StageController } from './stage.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Stage } from './entities/stage.entity';
-import { ChallongeModule } from '../challonge/challonge.module';
 
 @Module({
-  imports: [ChallongeModule, TypeOrmModule.forFeature([Stage])],
+  imports: [TypeOrmModule.forFeature([Stage])],
   controllers: [StageController],
   providers: [StageService]
 })
