d726258646b28856aa7bab6f64d81b2e60df380c
Ruben Perez <rub.per96@gmail.com>
2024-04-28 01:45:27 -0600

    dx: Use general constant for Challonge's base url

--- src/modules/challonge/match/match.service.ts
+++ src/modules/challonge/match/match.service.ts
@@ -5,6 +5,7 @@ import { firstValueFrom } from 'rxjs';
 
 import { Match, Matches } from './dto/match.response';
 import { UpdateMatchDto } from './dto/update-match.dto';
+import { CHALLONGE_API_URL } from '../auth/auth.service';
 
 export enum MatchState {
   Reopen = 'reopen',
@@ -18,23 +19,23 @@ export class MatchService {
   constructor(private httpService: HttpService) {}
 
   async getMatch(tournamentId: string, matchId: string) {
-    const response = this.httpService.get<Match>(`https://api.challonge.com/v2/tournaments/${tournamentId}/matches/${matchId}.json`);
+    const response = this.httpService.get<Match>(`${CHALLONGE_API_URL}/tournaments/${tournamentId}/matches/${matchId}.json`);
     return (await firstValueFrom(response)).data;
   }
 
   async getMatches(tournamentId: string) {
-    const response = this.httpService.get<Matches>(`https://api.challonge.com/v2/tournaments/${tournamentId}/matches.json`);
+    const response = this.httpService.get<Matches>(`${CHALLONGE_API_URL}/tournaments/${tournamentId}/matches.json`);
     return (await firstValueFrom(response)).data;
   }
 
   async updateMatch(tournamentId: string, matchId: string, updateMatchDto: UpdateMatchDto) {
-    const response = this.httpService.put<Match>(`https://api.challonge.com/v2/tournaments/${tournamentId}/matches/${matchId}.json`, updateMatchDto);
+    const response = this.httpService.put<Match>(`${CHALLONGE_API_URL}/tournaments/${tournamentId}/matches/${matchId}.json`, updateMatchDto);
     return (await firstValueFrom(response)).data;
   }
 
   async updateMatchState(tournamentId: string, matchId: string, state: MatchState) {
     const response = this.httpService.put<Match>(
-      `https://api.challonge.com/v2/tournaments/${tournamentId}/matches/${matchId}/change_state.json`,
+      `${CHALLONGE_API_URL}/tournaments/${tournamentId}/matches/${matchId}/change_state.json`,
       { data: { type: 'MatchState', attributes: { state } } }
     );
     return (await firstValueFrom(response)).data;
